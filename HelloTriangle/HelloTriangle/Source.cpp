e99a54aec59663531a99928600d0bb1a546a33d6
luxiang <luxiang@htgames.cn>
2017-11-14 15:59:47 +0800

    draw rectange

--- HelloTriangle/HelloTriangle/Source.cpp
+++ HelloTriangle/HelloTriangle/Source.cpp
@@ -81,9 +81,15 @@ int main(int argc, char* argv[])
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
-		-0.5f,-0.5f,0.0f,
+		0.5f,0.5f,0.0f,
 		0.5f,-0.5f,0.0f,
-		0.0f,0.5f,0.0f
+		-0.5f,-0.5f,0.0f,
+		-0.5f,0.5f,0.0f
+	};
+
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
 	};
 
 	/* VAO */
@@ -94,9 +100,15 @@ int main(int argc, char* argv[])
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -113,7 +125,8 @@ int main(int argc, char* argv[])
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
