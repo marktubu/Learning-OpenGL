900d871aa0630a2a2514593b061a2e6b22a086a1
mark <mark@tubu.com>
2022-09-14 13:46:14 +0800

    add model mesh class

---
+++ Windows/Lighting/Lighting/mesh.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.h"
+
+using namespace std;
+
+#define MAX_BONE_INFLUENCE 4
+
+struct Vertex
+{
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+	glm::vec3 Tangent;
+	glm::vec3 Bitangent;
+	int m_BoneIDs[MAX_BONE_INFLUENCE];
+	float m_Weights[MAX_BONE_INFLUENCE];
+};
+
+struct Texture
+{
+	unsigned int id;
+	string type;
+	aiString path;
+};
+
+class Mesh {
+public:
+	vector<Vertex> vertices;
+	vector<unsigned int> indices;
+	vector<Texture> textures;
+
+	Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures) {
+		this->vertices = vertices;
+		this->indices = indices;
+		this->textures = textures;
+
+		setupMesh();
+	}
+	void Draw(Shader shader) {
+		shader.use();
+		unsigned int Nr_Diffuse = 1;
+		unsigned int Nr_Specular = 1;
+		unsigned int Nr_Normal = 1;
+		unsigned int Nr_Height = 1;
+		for (int i = 0;i < textures.size();i++) {
+			Texture texture = textures[i];
+			string texture_name = texture.type;
+			string number;
+			if (texture_name == "texture_diffuse") {
+				number = std::to_string(Nr_Diffuse++);
+			}
+			else if (texture_name == "texture_specular") {
+				number = std::to_string(Nr_Specular++);
+			}
+			else if (texture_name == "texture_normal") {
+				number = std::to_string(Nr_Normal++);
+			}
+			else if (texture_name == "texture_height") {
+				number = std::to_string(Nr_Height++);
+			}
+			glActiveTexture(GL_TEXTURE0 + i);
+			shader.setInt(("material." + texture_name + number).c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, texture.id);
+		}
+		glActiveTexture(GL_TEXTURE0);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
+
+private:
+	unsigned int VAO, VBO, EBO;
+	void setupMesh() {
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, false, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+		// vertex tangent
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Tangent));
+		// vertex bitangent
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Bitangent));
+		// ids
+		glEnableVertexAttribArray(5);
+		glVertexAttribIPointer(5, 4, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, m_BoneIDs));
+
+		// weights
+		glEnableVertexAttribArray(6);
+		glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, m_Weights));
+
+		glBindVertexArray(0);
+	}
+};
